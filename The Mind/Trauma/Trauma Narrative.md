0102dac49d49cc3923ba3a5bc2fef26e0e2ec051
algorusty <3823121+algorusty@users.noreply.github.com>
2023-11-18 07:31:52 -0600

    Update css, add trauma

---
+++ The Mind/Trauma/Trauma Narrative.md
@@ -0,0 +1,36 @@
+# Trauma Narrative
+
+| **Introduction**                          | **Context Before the Event**                      |
+|-------------------------------------------|---------------------------------------------------|
+| **Date of Writing**:                      | **Date of Event**:                                |
+| **Current Feelings**:                     | **Location**:                                     |
+| **Intention for This Narrative**:         | **People Involved**:                              |
+|                                           | **Life Before the Event**:                        |
+|                                           | **Initial Feelings and Thoughts**:                |
+
+| **Detailed Account of the Event**         | **Aftermath**                                     |
+|-------------------------------------------|---------------------------------------------------|
+| **Chronological Description**:            | **Initial Response Post-Event**:                  |
+|   - Beginning:                            | **Short-Term Effects**:                           |
+|   - Middle:                               | **Long-Term Effects**:                            |
+|   - End:                                  |                                                   |
+| **Sensory Details**:                      |                                                   |
+|   - Visual:                               |                                                   |
+|   - Auditory:                             |                                                   |
+|   - Olfactory (Smell):                    |                                                   |
+|   - Tactile (Touch):                      |                                                   |
+|   - Emotional:                            |                                                   |
+| **Thoughts During the Event**:            |                                                   |
+| **Immediate Reaction**:                   |                                                   |
+
+| **Personal Reflections**                  | **Coping and Healing**                            |
+|-------------------------------------------|---------------------------------------------------|
+| **Emotions Felt While Recounting**:       | **Coping Mechanisms Used**:                       |
+| **New Insights/Perspectives Gained**:     | **Support Systems**:                              |
+| **Unacknowledged Aspects of Impact**:     | **Personal Growth and Strengths Discovered**:     |
+
+| **Looking Forward**                       | **Conclusion**                                    |
+|-------------------------------------------|---------------------------------------------------|
+| **Goals for Healing and Development**:    | **Summary of Insights and Growth**:               |
+| **Positive Changes to Implement**:        | **Final Thoughts on Narrative Process**:          |
+| **Affirmations for the Future**:          |                                                   |
--- index.ts
+++ index.ts
@@ -47,11 +47,10 @@ const generateNavbar = (filePaths: string[]): string => {
         }
     });
 
-    let navbar = '<ul id="navbar">';
+    let navbar = '<ul id="navbar" class="markdown-navbar">';
     Object.keys(folderStructure).forEach(folder => {
         navbar += `<li class="folder"><span class="folder-name">${folder}</span><ul class="dropdown">`;
         folderStructure[folder].forEach(file => {
-            // Remove the ".md" extension from the display name
             const displayName = file.replace('.md', '');
             navbar += `<li><a href="#${path.basename(file, '.md')}">${displayName}</a></li>`;
         });
@@ -63,53 +62,100 @@ const generateNavbar = (filePaths: string[]): string => {
 };
 
 
+
 const buildHTMLPage = (htmlContent: string, files: string[]): string => {
     const navbar = generateNavbar(files);
     return `
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Generated Website</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
-    <style>
-        body { font-family: Arial, sans-serif; display: flex; }
-        #navbar { list-style-type: none; padding: 0; width: 200px; background-color: #f0f0f0; }
-        #navbar .folder-name { cursor: pointer; display: block; padding: 8px; }
-        #navbar .folder-name:before { content: '▸'; display: inline-block; margin-right: 5px; }
-        #navbar .dropdown { display: none; list-style-type: none; padding: 0; margin: 0; }
-        #navbar li.folder.open .dropdown { display: block; }
-        #navbar li.folder.open .folder-name:before { content: '▾'; }
-        #content { flex-grow: 1; padding: 20px; }
-        .content { display: none; }
-        .content.active { display: block; }
-    </style>
-</head>
-<body>
-    ${navbar}
-    <div id="content">${htmlContent}</div>
-    <script>
-        document.querySelectorAll('#navbar .folder-name').forEach(element => {
-            element.addEventListener('click', function() {
-                this.parentElement.classList.toggle('open');
-            });
-        });
-        document.getElementById('navbar').addEventListener('click', function(e) {
-            if (e.target.tagName === 'A' && e.target.getAttribute('href').startsWith('#')) {
-                var active = document.querySelector('.content.active');
-                if (active) active.classList.remove('active');
-                var contentId = e.target.getAttribute('href').substring(1);
-                if (contentId) {
-                    var content = document.getElementById(contentId);
-                    if (content) content.classList.add('active');
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>The Mind</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.4.0/github-markdown.min.css" integrity="sha512-30lMQ13MJJk66BfdlnvVnKmP05V7Qt1g6sHyYigDgV8i9M2ENAsXk1U4dVvKUYB6pqb2bVhoxhZsYK08hQpS+g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <style>
+                body { font-family: Arial, sans-serif; display: flex; }
+                #navbar { list-style-type: none; padding: 0; width: 200px; background-color: #f0f0f0; }
+                #navbar .folder-name { cursor: pointer; display: block; padding: 8px; }
+                #navbar .folder-name:before { content: '▸'; display: inline-block; margin-right: 5px; }
+                #navbar .dropdown { display: none; list-style-type: none; padding: 0; margin: 0; }
+                #navbar li.folder.open .dropdown { display: block; }
+                #navbar li.folder.open .folder-name:before { content: '▾'; }
+                #content { flex-grow: 1; padding: 20px; }
+                .content { display: none; }
+                .content.active { display: block; }
+                // .markdown-body { max-width: 800px; margin: 0 auto; padding: 45px; }
+                .markdown-navbar {
+                    font-family: Arial, sans-serif;
+                    background-color: #f6f8fa;
+                    border-right: 1px solid #e1e4e8;
+                    padding: 0.5em;
+                    margin: 0;
+                    width: 200px;
                 }
-                e.preventDefault();
-            }
-        });
-    </script>
-</body>
-</html>
+            
+                .markdown-navbar ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                }
+            
+                .markdown-navbar li a {
+                    color: #0366d6;
+                    text-decoration: none;
+                    padding: 0.5em;
+                    display: block;
+                }
+            
+                .markdown-navbar li a:hover {
+                    text-decoration: underline;
+                }
+            
+                .folder-name {
+                    cursor: pointer;
+                    font-weight: bold;
+                }
+            
+                .folder-name:before {
+                    content: '▸';
+                    display: inline-block;
+                    margin-right: 5px;
+                }
+            
+                .folder.open .dropdown {
+                    display: block;
+                }
+            
+                .folder.open .folder-name:before {
+                    content: '▾';
+                }
+            </style>
+        </head>
+        <body>
+            ${navbar}
+            <div id="content" class="markdown-body">${htmlContent}</div>
+            <script>
+                document.querySelectorAll('#navbar .folder-name').forEach(element => {
+                    element.addEventListener('click', function() {
+                        this.parentElement.classList.toggle('open');
+                    });
+                });
+                document.getElementById('navbar').addEventListener('click', function(e) {
+                    if (e.target.tagName === 'A' && e.target.getAttribute('href').startsWith('#')) {
+                        var active = document.querySelector('.content.active');
+                        if (active) active.classList.remove('active');
+                        var contentId = e.target.getAttribute('href').substring(1);
+                        if (contentId) {
+                            var content = document.getElementById(contentId);
+                            if (content) content.classList.add('active');
+                        }
+                        e.preventDefault();
+                    }
+                });
+            </script>
+        </body>
+        </html>
     `.trim();
 };
+
 
 const rootFolder = path.resolve('./The Mind'); // Set your root folder
 const markdownFiles = scanDirectoryStructure(rootFolder);
